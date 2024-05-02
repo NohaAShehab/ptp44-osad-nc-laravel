55c8025143299f872ad9ceb31ad000397bade39d
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-05-02 11:41:25 +0300

    using gates in blade file

--- demo/resources/views/tracks/index.blade.php
+++ demo/resources/views/tracks/index.blade.php
@@ -22,14 +22,21 @@
                 <td> {{$track->owner ? $track->owner->name : "default"}}</td>
                 <td> <a href="{{route('tracks.show', $track->id)}}" class="btn btn-info">Show  </a></td>
 
-                <td> <a href="{{route('tracks.edit', $track->id)}}" class="btn btn-warning">Edit  </a></td>
-                <td>
-                    <form method="post" action="{{route('tracks.destroy', $track->id)}}">
-                        @method('delete')
-                        @csrf
-                        <input type="submit" value="Delete" class="btn btn-danger">
-                    </form>
-                </td>
+
+                @can('track_update_delete', $track)
+                    <td> <a href="{{route('tracks.edit', $track->id)}}" class="btn btn-warning">Edit  </a></td>
+                    <td>
+                        <form method="post" action="{{route('tracks.destroy', $track->id)}}">
+                            @method('delete')
+                            @csrf
+                            <input type="submit" value="Delete" class="btn btn-danger">
+                        </form>
+                    </td>
+                @else
+                    <td> You are not authorized to edit </td>
+                    <td> You are not authorized to delete </td>
+                @endcan
+
             </tr>
 
         @endforeach
